42174b5f0ea939fbc3f0ffa87e7f34f16d998b62
Simo Echaarani <echaaranimohamed@gmail.com>
2024-03-09 20:38:31 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((res) => {
-      console.log('yay from then', res);
-    })
-    .catch(() => {});
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') return { status: result.status, value: result.value };
+
+    return { status: result.status, value: result.reason.message };
+  }));
 }
