1530f448236cf64245aa3db6662341b33873b6af
Simo Echaarani <echaaranimohamed@gmail.com>
2024-03-11 03:09:09 +0000

    eslint fix

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,15 +3,19 @@ export default class Currency {
     this.code = code;
     this.name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(value) {
     this._code = value;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     this._name = value;
   }
