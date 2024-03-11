f60d983869f5cf1a9538ed2323875f61817e1804
Simo Echaarani <echaaranimohamed@gmail.com>
2024-03-11 02:42:53 +0000

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,58 +1,43 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this.checkNameType(name);
-    this.checkLengthType(length);
-    this.checkStudentsType(students);
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this.checkNameType(value);
-    this._name = value;
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    this.checkLengthType(value);
-    this._length = value;
+  set students(students) {
+    if (
+      !Array.isArray(students)
+      || !students.every((student) => typeof student === 'string')
+    ) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
   }
 
   get students() {
     return this._students;
-  }
-
-  set students(value) {
-    this.checkStudentsType(value);
-    this._students = value;
-  }
-
-  static checkNameType(value) {
-    if (typeof value !== 'string') throw new TypeError('Name must be a string');
-  }
-
-  static checkLengthType(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a string');
-    }
-  }
-
-  static checkStudentsType(value) {
-    if (!Array.isArray(value)) {
-      throw new TypeError('Students must be an array');
-    }
-
-    if (!value.every((element) => typeof element === 'string')) {
-      throw new Error('Students array must contain only strings');
-    }
   }
 }
