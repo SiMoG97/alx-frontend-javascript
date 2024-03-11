c71352b49a7242800e5e84bcb9819a8c2a532f9d
Simo Echaarani <echaaranimohamed@gmail.com>
2024-03-11 19:31:38 +0000

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+export default class EvCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    return new super.constructor[Symbol.species]();
+  }
+}
